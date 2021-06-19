63fe77a7a74238c67f0b4589eb0607ee6834775c
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-19 16:40:13 +0900

    update

--- react/src/components/TaskAndTimer/TaskAndTimer.jsx
+++ react/src/components/TaskAndTimer/TaskAndTimer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TodoList from "./TodoList";
 
 const TaskAndTimer = () => {
-  return <></>;
+  return (
+    <>
+      <TodoList />
+    </>
+  );
 };
 
 export default TaskAndTimer;
