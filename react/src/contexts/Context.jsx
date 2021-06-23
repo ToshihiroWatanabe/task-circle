93ea4758d2d048fbd14cf2eba977dc0ac089acb7
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-23 16:28:44 +0900

    コメントを追加

--- react/src/contexts/Context.jsx
+++ react/src/contexts/Context.jsx
@@ -5,40 +5,38 @@ export const Context = createContext([{}, () => {}]);
 /**
  * グローバルstateを提供します。
  * @param {*} props
- * @returns
  */
 export function ContextProvider(props) {
-  /** グローバルstate */
   const [state, setState] = useState({
-    // Slackに投稿するときの名前
+    /** Slackに投稿するときの名前 */
     slackUserName: "",
-    // Slack Webhook URL
+    /** Slack Webhook URL */
     slackWebhookUrl: "",
-    // ユーザーID
+    /** ユーザーID */
     userId: "",
-    // パスワード
+    /** パスワード */
     password: "",
-    // 日報ID
+    /** 日報ID */
     reportId: "",
-    // 日報の更新日時
+    /** 日報の更新日時 */
     reportUpdatedAt: "",
-    // 日報
+    /** 日報 */
     reports: [],
-    // 作成ダイアログ待機中の日報
+    /** 作成ダイアログ待機中の日報 */
     waitingReport: {},
-    // タイマーがオンかどうか
+    /** タイマーがオンかどうか */
     isTimerOn: false,
-    // ポモドーロモードがオンかどうか
+    /** ポモドーロモードがオンかどうか */
     isPomodoroEnabled: false,
-    // 休憩を自動スタートするかどうか
+    /** 休憩を自動スタートするかどうか */
     isBreakAutoStart: true,
-    // 作業タイマーの時間(秒)
+    /** 作業タイマーの時間(秒) */
     workTimerLength: 25 * 60,
-    // 休憩タイマーの時間(秒)
+    /** 休憩タイマーの時間(秒) */
     breakTimerLength: 5 * 60,
-    // ポモドーロタイマーの残り時間
+    /** ポモドーロタイマーの残り時間 */
     pomodoroTimeLeft: 25 * 60,
-    // ポモドーロタイマーのタイプ
+    /** ポモドーロタイマーのタイプ */
     pomodoroTimerType: "work",
   });
 
