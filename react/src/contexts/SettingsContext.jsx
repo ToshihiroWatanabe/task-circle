ab3851182de285ab729fd68d1fb50486e0a5a7e7
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-23 16:28:53 +0900

    add: 設定のグローバルstateを追加

---
+++ react/src/contexts/SettingsContext.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from "react";
+
+export const SettingsContext = createContext([{}, () => {}]);
+
+/**
+ * 設定のグローバルstateを提供します。
+ * @param {*} props
+ */
+export function SettingsContextProvider(props) {
+  // 設定
+  const [settings, setSettings] = useState({
+    /** 作業用BGMのURL */
+    workVideoUrl: "",
+    /** 作業用BGMの音量(%) */
+    workVideoVolume: 100,
+    /** 休憩用BGMのURL */
+    breakVideoUrl: "",
+    /** 休憩用BGMの音量(%) */
+    breakVideoVolume: 100,
+    /** チクタク音の音量(%) */
+    tickVolume: 1,
+    /** 開始・停止・アラーム音量(%) */
+    volume: 100,
+  });
+
+  return (
+    <SettingsContext.Provider value={[settings, setSettings]}>
+      {props.children}
+    </SettingsContext.Provider>
+  );
+}
