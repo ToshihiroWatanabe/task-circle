ea80c1f799b0001ea8dd407f1caf40747960235c
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-28 21:00:02 +0900

    update: PCの横幅のときだけドラッグ移動可能にした

--- react/src/components/TaskAndTimer/FloatingTimer.jsx
+++ react/src/components/TaskAndTimer/FloatingTimer.jsx
@@ -1,15 +1,9 @@
 import React, { memo, useContext, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Fab from "@material-ui/core/Fab";
-import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import "./FloatingTimer.css";
-import { Context } from "contexts/Context";
 import { useTheme, Zoom, useMediaQuery } from "@material-ui/core";
-import StopIcon from "@material-ui/icons/Stop";
-import CircularDeterminate from "./CircularDeterminate";
-import { SettingsContext } from "contexts/SettingsContext";
-import { secondToHHMMSS, secondToHHMMSS_ja } from "utils/convert";
 import { Rnd } from "react-rnd";
+import TimerFab from "./TimerFab";
 
 const DEFAULT_WIDTH = 180;
 const DEFAULT_HEIGHT = 180;
@@ -25,102 +19,22 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
     },
   },
-  fab: {
-    width: "10rem",
-    height: "10rem",
-    display: "flex !important",
-    alignContent: "",
-    [theme.breakpoints.down("sm")]: {
-      width: "7.5rem",
-      height: "7.5rem",
-    },
-    [theme.breakpoints.down("xs")]: {
-      width: "4rem",
-      height: "4rem",
-    },
-  },
-  timerCount: {
-    [theme.breakpoints.up("md")]: {
-      fontSize: "2.75rem",
-      marginBottom: "-0.7rem",
-    },
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "2rem",
-      marginBottom: "-0.5rem",
-    },
-    [theme.breakpoints.down("xs")]: {
-      fontSize: "1rem",
-      marginBottom: "-0.5rem",
-    },
-  },
-  playStopIcon: {
-    fontSize: "2.75rem",
-    marginBottom: "-0.3rem",
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "2rem",
-      marginBottom: "-0.3rem",
-    },
-    [theme.breakpoints.down("xs")]: {
-      fontSize: "1.5rem",
-      marginBottom: "-0.5rem",
-    },
-  },
-  content: {
-    whiteSpace: "nowrap",
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
-  },
 }));
 
 const FloatingTimer = memo((props) => {
   const classes = useStyles();
   const theme = useTheme();
-  const [state] = useContext(Context);
-  const [settings] = useContext(SettingsContext);
+
   const useMediaQueryThemeBreakpointsUpMd = useMediaQuery(
     theme.breakpoints.up("md")
   );
   const useMediaQueryThemeBreakpointsDownSm = useMediaQuery(
     theme.breakpoints.down("sm")
   );
-  const useMediaQueryThemeBreakpointsDownXs = useMediaQuery(
-    theme.breakpoints.down("xs")
-  );
 
   const transitionDuration = {
     enter: theme.transitions.duration.enteringScreen,
     exit: theme.transitions.duration.leavingScreen,
-  };
-
-  const selectedTask =
-    Object.values(props.columns).filter((column, index) => {
-      return (
-        column.items.filter((item, index) => {
-          return item.isSelected;
-        })[0] !== undefined
-      );
-    }).length > 0
-      ? Object.values(props.columns)
-          .filter((column, index) => {
-            return (
-              column.items.filter((item, index) => {
-                return item.isSelected;
-              })[0] !== undefined
-            );
-          })[0]
-          .items.filter((item, index) => {
-            return item.isSelected;
-          })[0]
-      : null;
-
-  /**
-   * Fabがクリックされたときの処理です。
-   */
-  const onFabClick = () => {
-    if (!isDragging && selectedTask !== null) {
-      props.onPlayButtonClick(0, "fab");
-    }
   };
 
   const [positionX, setPositionX] = useState(
@@ -181,147 +95,62 @@ const FloatingTimer = memo((props) => {
 
   return (
     <>
-      {/* <div className={classes.root}> */}
-      {/* <Zoom
-        timeout={transitionDuration}
-        in={true}
-        style={{
-          transitionDelay: `${transitionDuration.exit}ms`,
-        }}
-        unmountOnExit
-      > */}
-      <Rnd
-        dragAxis="x"
-        minWidth={MIN_WIDTH}
-        minHeight={MIN_HEIGHT}
-        maxWidth={window.innerWidth * 0.9}
-        maxHeight={window.innerHeight * 0.9}
-        lockAspectRatio={true}
-        onResizeStop={onResizeStop}
-        onDrag={() => {
-          setIsDragging(true);
-        }}
-        onDragStop={onDragStop}
-        position={{ x: positionX, y: positionY }}
-        default={{
-          width: DEFAULT_WIDTH,
-          height: DEFAULT_HEIGHT,
-        }}
-        style={{
-          display: "flex",
-          posision: "fixed",
-          bottom: "0",
-          alignItems: "center",
-          justifyContent: "center",
-          border: "solid 1px #ddd",
-          borderRadius: "8px",
-          background: "#f0f0f0",
-          zIndex: "1",
-          padding: "8px",
-        }}
-      >
-        <Fab
-          color="primary"
-          aria-label="timer"
-          className={classes.fab}
-          id="floatingTimer"
-          onClick={() => {
-            onFabClick();
+      {useMediaQueryThemeBreakpointsDownSm && (
+        <div className={classes.root}>
+          <Zoom
+            timeout={transitionDuration}
+            in={true}
+            style={{
+              transitionDelay: `${transitionDuration.exit}ms`,
+            }}
+            unmountOnExit
+          >
+            <TimerFab
+              columns={props.columns}
+              isDragging={isDragging}
+              onPlayButtonClick={props.onPlayButtonClick}
+            />
+          </Zoom>
+        </div>
+      )}
+      {useMediaQueryThemeBreakpointsUpMd && (
+        <Rnd
+          dragAxis="x"
+          minWidth={MIN_WIDTH}
+          minHeight={MIN_HEIGHT}
+          maxWidth={window.innerWidth * 0.9}
+          maxHeight={window.innerHeight * 0.9}
+          lockAspectRatio={true}
+          onResizeStop={onResizeStop}
+          onDrag={() => {
+            setIsDragging(true);
+          }}
+          onDragStop={onDragStop}
+          position={{ x: positionX, y: positionY }}
+          default={{
+            width: DEFAULT_WIDTH,
+            height: DEFAULT_HEIGHT,
+          }}
+          style={{
+            display: "flex",
+            posision: "fixed",
+            bottom: "0",
+            alignItems: "center",
+            justifyContent: "center",
+            border: "solid 1px #ddd",
+            borderRadius: "8px",
+            background: "#f0f0f0",
+            zIndex: "1",
+            padding: "8px",
           }}
         >
-          {/* 進行状況サークル */}
-          <CircularDeterminate columns={props.columns} />
-          {/* カウント */}
-          <div className={classes.timerCount}>
-            {settings.isPomodoroEnabled &&
-              Math.floor(state.pomodoroTimeLeft / 60) +
-                ":" +
-                (Math.floor(state.pomodoroTimeLeft % 60) < 10
-                  ? "0" + Math.floor(state.pomodoroTimeLeft % 60)
-                  : Math.floor(state.pomodoroTimeLeft % 60))}
-            {/* ポモドーロモードじゃないとき */}
-            {!settings.isPomodoroEnabled && selectedTask !== null && (
-              <span
-                style={{
-                  fontSize:
-                    selectedTask.spentSecond > 3600
-                      ? useMediaQueryThemeBreakpointsUpMd
-                        ? "2rem"
-                        : useMediaQueryThemeBreakpointsDownXs
-                        ? "0.8rem"
-                        : useMediaQueryThemeBreakpointsDownSm
-                        ? "1.4rem"
-                        : ""
-                      : "",
-                }}
-              >
-                {selectedTask.spentSecond < 3600
-                  ? secondToHHMMSS(selectedTask.spentSecond).substring(3)
-                  : secondToHHMMSS(selectedTask.spentSecond)}
-              </span>
-            )}
-          </div>
-          {/* タスク名 */}
-          <div className={classes.content}>
-            {/* ポモドーロがオン かつ 作業タイマー かつ 選択しているタスクが存在する */}
-            {settings.isPomodoroEnabled &&
-            state.pomodoroTimerType === "work" &&
-            selectedTask !== null
-              ? selectedTask.content.length > 10
-                ? selectedTask.content.slice(0, 10) + "..."
-                : selectedTask.content
-              : ""}
-            {settings.isPomodoroEnabled &&
-              state.pomodoroTimerType === "work" &&
-              selectedTask === null && (
-                <>
-                  <p style={{ margin: "0" }}>タスクが選択</p>
-                  されていません
-                </>
-              )}
-            {settings.isPomodoroEnabled && state.pomodoroTimerType === "break"
-              ? "休憩"
-              : ""}
-            {/* ポモドーロモードじゃないとき */}
-            {!settings.isPomodoroEnabled &&
-              selectedTask !== null &&
-              selectedTask.estimatedSecond - selectedTask.spentSecond > 0 && (
-                <p style={{ margin: "0" }}>
-                  残り
-                  {secondToHHMMSS_ja(
-                    selectedTask.estimatedSecond - selectedTask.spentSecond
-                  )}
-                </p>
-              )}
-            {!settings.isPomodoroEnabled && selectedTask !== null
-              ? selectedTask.content.length > 10
-                ? selectedTask.content.slice(0, 10) + "..."
-                : selectedTask.content
-              : ""}
-            {!settings.isPomodoroEnabled && selectedTask === null && (
-              <>
-                <p style={{ margin: "0" }}>タスクが選択</p>
-                されていません
-              </>
-            )}
-          </div>
-          {/* 再生・停止アイコン */}
-          <div>
-            {selectedTask !== null && !state.isTimerOn && (
-              <>
-                <PlayArrowIcon className={classes.playStopIcon} />
-              </>
-            )}
-            {selectedTask !== null && state.isTimerOn && (
-              <>
-                <StopIcon className={classes.playStopIcon} />
-              </>
-            )}
-          </div>
-        </Fab>
-      </Rnd>
-      {/* </Zoom> */}
-      {/* </div> */}
+          <TimerFab
+            columns={props.columns}
+            isDragging={isDragging}
+            onPlayButtonClick={props.onPlayButtonClick}
+          />
+        </Rnd>
+      )}
     </>
   );
 });
---
+++ react/src/components/TaskAndTimer/TimerFab.jsx
@@ -0,0 +1,220 @@
+import React, { memo, useContext, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import "./FloatingTimer.css";
+import { Context } from "contexts/Context";
+import { useTheme, useMediaQuery } from "@material-ui/core";
+import StopIcon from "@material-ui/icons/Stop";
+import CircularDeterminate from "./CircularDeterminate";
+import { SettingsContext } from "contexts/SettingsContext";
+import { secondToHHMMSS, secondToHHMMSS_ja } from "utils/convert";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "fixed",
+    bottom: 0,
+    right: 0,
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  fab: {
+    width: "10rem",
+    height: "10rem",
+    display: "flex !important",
+    alignContent: "",
+    [theme.breakpoints.down("sm")]: {
+      width: "7.5rem",
+      height: "7.5rem",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "4rem",
+      height: "4rem",
+    },
+  },
+  timerCount: {
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2.75rem",
+      marginBottom: "-0.7rem",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2rem",
+      marginBottom: "-0.5rem",
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      marginBottom: "-0.5rem",
+    },
+  },
+  playStopIcon: {
+    fontSize: "2.75rem",
+    marginBottom: "-0.3rem",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2rem",
+      marginBottom: "-0.3rem",
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1.5rem",
+      marginBottom: "-0.5rem",
+    },
+  },
+  content: {
+    whiteSpace: "nowrap",
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+}));
+
+const TimerFab = memo((props) => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [state] = useContext(Context);
+  const [settings] = useContext(SettingsContext);
+  const useMediaQueryThemeBreakpointsUpMd = useMediaQuery(
+    theme.breakpoints.up("md")
+  );
+  const useMediaQueryThemeBreakpointsDownSm = useMediaQuery(
+    theme.breakpoints.down("sm")
+  );
+  const useMediaQueryThemeBreakpointsDownXs = useMediaQuery(
+    theme.breakpoints.down("xs")
+  );
+
+  const selectedTask =
+    Object.values(props.columns).filter((column, index) => {
+      return (
+        column.items.filter((item, index) => {
+          return item.isSelected;
+        })[0] !== undefined
+      );
+    }).length > 0
+      ? Object.values(props.columns)
+          .filter((column, index) => {
+            return (
+              column.items.filter((item, index) => {
+                return item.isSelected;
+              })[0] !== undefined
+            );
+          })[0]
+          .items.filter((item, index) => {
+            return item.isSelected;
+          })[0]
+      : null;
+
+  /**
+   * Fabがクリックされたときの処理です。
+   */
+  const onFabClick = () => {
+    if (!props.isDragging && selectedTask !== null) {
+      props.onPlayButtonClick(0, "fab");
+    }
+  };
+
+  return (
+    <>
+      <Fab
+        color="primary"
+        aria-label="timer"
+        className={classes.fab}
+        id="floatingTimer"
+        onClick={() => {
+          onFabClick();
+        }}
+      >
+        {/* 進行状況サークル */}
+        <CircularDeterminate columns={props.columns} />
+        {/* カウント */}
+        <div className={classes.timerCount}>
+          {settings.isPomodoroEnabled &&
+            Math.floor(state.pomodoroTimeLeft / 60) +
+              ":" +
+              (Math.floor(state.pomodoroTimeLeft % 60) < 10
+                ? "0" + Math.floor(state.pomodoroTimeLeft % 60)
+                : Math.floor(state.pomodoroTimeLeft % 60))}
+          {/* ポモドーロモードじゃないとき */}
+          {!settings.isPomodoroEnabled && selectedTask !== null && (
+            <span
+              style={{
+                fontSize:
+                  selectedTask.spentSecond > 3600
+                    ? useMediaQueryThemeBreakpointsUpMd
+                      ? "2rem"
+                      : useMediaQueryThemeBreakpointsDownXs
+                      ? "0.8rem"
+                      : useMediaQueryThemeBreakpointsDownSm
+                      ? "1.4rem"
+                      : ""
+                    : "",
+              }}
+            >
+              {selectedTask.spentSecond < 3600
+                ? secondToHHMMSS(selectedTask.spentSecond).substring(3)
+                : secondToHHMMSS(selectedTask.spentSecond)}
+            </span>
+          )}
+        </div>
+        {/* タスク名 */}
+        <div className={classes.content}>
+          {/* ポモドーロがオン かつ 作業タイマー かつ 選択しているタスクが存在する */}
+          {settings.isPomodoroEnabled &&
+          state.pomodoroTimerType === "work" &&
+          selectedTask !== null
+            ? selectedTask.content.length > 10
+              ? selectedTask.content.slice(0, 10) + "..."
+              : selectedTask.content
+            : ""}
+          {settings.isPomodoroEnabled &&
+            state.pomodoroTimerType === "work" &&
+            selectedTask === null && (
+              <>
+                <p style={{ margin: "0" }}>タスクが選択</p>
+                されていません
+              </>
+            )}
+          {settings.isPomodoroEnabled && state.pomodoroTimerType === "break"
+            ? "休憩"
+            : ""}
+          {/* ポモドーロモードじゃないとき */}
+          {!settings.isPomodoroEnabled &&
+            selectedTask !== null &&
+            selectedTask.estimatedSecond - selectedTask.spentSecond > 0 && (
+              <p style={{ margin: "0" }}>
+                残り
+                {secondToHHMMSS_ja(
+                  selectedTask.estimatedSecond - selectedTask.spentSecond
+                )}
+              </p>
+            )}
+          {!settings.isPomodoroEnabled && selectedTask !== null
+            ? selectedTask.content.length > 10
+              ? selectedTask.content.slice(0, 10) + "..."
+              : selectedTask.content
+            : ""}
+          {!settings.isPomodoroEnabled && selectedTask === null && (
+            <>
+              <p style={{ margin: "0" }}>タスクが選択</p>
+              されていません
+            </>
+          )}
+        </div>
+        {/* 再生・停止アイコン */}
+        <div>
+          {selectedTask !== null && !state.isTimerOn && (
+            <>
+              <PlayArrowIcon className={classes.playStopIcon} />
+            </>
+          )}
+          {selectedTask !== null && state.isTimerOn && (
+            <>
+              <StopIcon className={classes.playStopIcon} />
+            </>
+          )}
+        </div>
+      </Fab>
+    </>
+  );
+});
+
+export default TimerFab;
