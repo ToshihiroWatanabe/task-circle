8ec183e31302b3e6600542433e6eb194d0344e86
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-28 14:13:41 +0900

    update

--- react/src/components/TaskAndTimer/FooterTimer.jsx
+++ react/src/components/TaskAndTimer/FooterTimer.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Rnd } from "react-rnd";
 
-const DEFAULT_WIDTH = 320;
-const DEFAULT_HEIGHT = 180;
+const DEFAULT_WIDTH = 360;
+const DEFAULT_HEIGHT = 144;
+const MIN_WIDTH = 100;
+const MIN_HEIGHT = 100;
 
 const style = {
   display: "flex",
@@ -11,6 +13,7 @@ const style = {
   alignItems: "center",
   justifyContent: "center",
   border: "solid 1px #ddd",
+  borderRadius: "4px",
   background: "#f0f0f0",
   zIndex: "9999",
 };
@@ -23,35 +26,50 @@ const FooterTimer = () => {
     document.documentElement.clientWidth / 2 - DEFAULT_WIDTH / 2
   );
   const [positionY, setPositionY] = useState(
-    document.documentElement.clientHeight - DEFAULT_HEIGHT * 1.5
+    document.documentElement.clientHeight - DEFAULT_HEIGHT - DEFAULT_HEIGHT / 2
   );
   const [height, setHeight] = useState(DEFAULT_HEIGHT);
   const [width, setWidth] = useState(DEFAULT_WIDTH);
   const onResizeStop = (e, dir, refToElement, delta, position) => {
+    let count = 0;
     setWidth((width) => {
-      setHeight((height) => {
-        width = width + delta.width;
-        height = height + delta.height;
-        setPositionX((positionX) => {
-          return positionX - delta.width / 2 > 0
-            ? positionX - delta.width / 2
-            : 0;
+      setPositionX((positionX) => {
+        setHeight((height) => {
+          if (count === 0) {
+            height =
+              height + delta.height > window.innerHeight * 0.9
+                ? window.innerHeight * 0.9
+                : height + delta.height;
+            setPositionY(
+              document.documentElement.clientHeight -
+                height -
+                DEFAULT_HEIGHT / 2
+            );
+            count++;
+          }
+          return height;
         });
-        setPositionY(
-          document.documentElement.clientHeight - height - DEFAULT_HEIGHT / 2
-        );
-        return height;
+        return position.x > window.innerWidth * 0.9
+          ? window.innerWidth * 0.9
+          : position.x;
       });
+      console.log(width, delta.width);
+      width =
+        width + delta.width > window.innerWidth * 0.9
+          ? window.innerWidth * 0.9
+          : width + delta.width;
+      console.log(width);
       return width;
     });
   };
 
   const onDragStop = (e, d) => {
+    console.log(width);
     setPositionX(
       d.x > 0
-        ? d.x < window.parent.screen.width - width
+        ? d.x < window.innerWidth - width
           ? d.x
-          : window.parent.screen.width - width
+          : window.innerWidth - width
         : 0
     );
   };
@@ -60,8 +78,10 @@ const FooterTimer = () => {
       <Rnd
         style={style}
         dragAxis="x"
-        minWidth="100px"
-        minHeight="100px"
+        minWidth={MIN_WIDTH}
+        minHeight={MIN_HEIGHT}
+        maxWidth={window.innerWidth * 0.9}
+        maxHeight={window.innerHeight * 0.9}
         // lockAspectRatio={true}
         onResizeStop={onResizeStop}
         onDragStop={onDragStop}
