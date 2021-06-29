13f7522e4dabca241b08ac9a7e45786c8c13979a
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-29 11:55:15 +0900

    add: Spring Initializr

---
+++ springboot/src/main/java/app/taskcircle/DemoApplication.java
@@ -0,0 +1,13 @@
+package app.taskcircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
