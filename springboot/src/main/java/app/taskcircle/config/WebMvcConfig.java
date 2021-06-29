edc13e720a7f9f25db9b8d4bc985385c610677a8
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-29 16:48:40 +0900

    update: WebSocket接続に成功

--- springboot/src/main/java/app/taskcircle/config/WebMvcConfig.java
+++ springboot/src/main/java/app/taskcircle/config/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000", "http://localhost:8161")
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000", "http://localhost:8161/")
                 .allowedHeaders(CorsConfiguration.ALL).allowedMethods("*");
     }
 }
--- springboot/src/main/java/app/taskcircle/controller/IndexController.java
+++ springboot/src/main/java/app/taskcircle/controller/IndexController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}",
-            "/{x:^(?!websocket$).*$}/**/{y:[\\w\\-]+}" })
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api|websocket$).*$}/**/{y:[\\w\\-]+}", })
     public String getIndex(HttpServletRequest request) {
         return "/index.html";
     }
