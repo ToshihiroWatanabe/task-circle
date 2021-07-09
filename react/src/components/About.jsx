c1aade672d0fffc91a898f2ff191748f637085d5
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-07-09 09:52:46 +0900

    update: サーバーのビルド時刻を返すようにした

--- react/src/components/About.jsx
+++ react/src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Card,
   IconButton,
@@ -10,6 +10,7 @@ import {
 import GitHubIcon from "@material-ui/icons/GitHub";
 import format from "date-fns/format";
 import preval from "preval.macro";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -27,26 +28,71 @@ const useStyles = makeStyles((theme) => ({
  */
 const About = () => {
   const classes = useStyles();
+  const [buildTimestamp, setBuildTimestamp] = useState(new Date(0));
+
+  useEffect(() => {
+    axios
+      .create({
+        // リクエスト送信先のURL
+        baseURL:
+          process.env.NODE_ENV === "development" ? "http://localhost:8160" : "",
+        // ヘッダーでタイプをJSONに指定
+        headers: {
+          "Content-type": "application/json",
+        },
+      })
+      .get("/actuator/info")
+      .then((response) => {
+        setBuildTimestamp(new Date(response.data.application.buildTimestamp));
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, []);
 
   return (
     <>
       <Card className={classes.card}>
         <Typography variant="h5">TaskCircle（タスクサークル）</Typography>
-        <Typography variant="caption">
-          ビルド時刻{" "}
-          {format(preval`module.exports = Date.now();`, "yyyy/MM/dd HH:mm:ss")}
-          <Tooltip title="GitHubでリポジトリを見る">
-            <Link
-              href="https://github.com/ToshihiroWatanabe/task-circle"
-              target="_blank"
-              rel="noopener"
-            >
-              <IconButton size="small" style={{ color: "black" }}>
-                <GitHubIcon />
-              </IconButton>
-            </Link>
-          </Tooltip>
-        </Typography>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Typography variant="caption">
+            クライアント側のビルド時刻{" "}
+            {format(
+              preval`module.exports = Date.now();`,
+              "yyyy/MM/dd HH:mm:ss"
+            )}
+          </Typography>
+          <Typography
+            variant="caption"
+            style={{ display: buildTimestamp.getTime() > 0 ? "" : "none" }}
+          >
+            サーバー側のビルド時刻{"　　 "}
+            {format(buildTimestamp, "yyyy/MM/dd HH:mm:ss")}
+          </Typography>
+          {/* 最新のバージョンかどうか */}
+          <div>
+            {buildTimestamp.getTime() - preval`module.exports = Date.now();` <=
+              3 * 60 * 1000 &&
+            buildTimestamp.getTime() - preval`module.exports = Date.now();` >= 0
+              ? "お使いのアプリは最新のバージョンです😀"
+              : ""}
+            {buildTimestamp.getTime() - preval`module.exports = Date.now();` >=
+            60 * 60 * 1000
+              ? "キャッシュを削除すると最新版がダウンロードされます。"
+              : ""}
+          </div>
+        </div>
+        <Tooltip title="GitHubでリポジトリを見る">
+          <Link
+            href="https://github.com/ToshihiroWatanabe/task-circle"
+            target="_blank"
+            rel="noopener"
+          >
+            <IconButton size="small" style={{ color: "black" }}>
+              <GitHubIcon />
+            </IconButton>
+          </Link>
+        </Tooltip>
 
         <Typography style={{ margin: "1rem 0" }}>
           Copyright © {new Date().getFullYear()} ワタナベトシヒロ All Rights
