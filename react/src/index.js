53b84275a2d6de9e1c03da47ef400eed9ce44d43
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-07-15 11:31:10 +0900

    update

--- react/src/index.js
+++ react/src/index.js
@@ -1,16 +1,16 @@
 import { CssBaseline } from "@material-ui/core";
 import App from "App";
 import ErrorBoundary from "components/ErrorBoundary";
-import { SessionsContextProvider } from "contexts/SessionsContext";
-import { SettingsContextProvider } from "contexts/SettingsContext";
-import { StateContextProvider } from "contexts/StateContext";
-import { StatisticsContextProvider } from "contexts/StatisticsContext";
-import { TodoListsContextProvider } from "contexts/TodoListsContext";
+import { SessionsContextProvider } from "contexts/SessionsContext.tsx";
+import { SettingsContextProvider } from "contexts/SettingsContext.tsx";
+import { StateContextProvider } from "contexts/StateContext.tsx";
+import { StatisticsContextProvider } from "contexts/StatisticsContext.tsx";
+import { TodoListsContextProvider } from "contexts/TodoListsContext.tsx";
 import "index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "reportWebVitals";
+import reportWebVitals from "reportWebVitals.ts";
 import * as serviceWorkerRegistration from "serviceWorkerRegistration";
 
 ReactDOM.render(
