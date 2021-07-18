7edbc9d5cd89d7100e6c1d711c58ffa9fd1a3a00
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-07-18 18:02:05 +0900

    update: プライバシーポリシーの本文のPCでの幅を大きくした

--- react/src/components/PrivacyPolicy.tsx
+++ react/src/components/PrivacyPolicy.tsx
@@ -10,6 +10,14 @@ const useStyles = makeStyles((theme) => ({
       width: "calc(100% - 8px)",
     },
   },
+  content: {
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "800px",
+    },
+  },
 }));
 
 /**
@@ -21,15 +29,17 @@ const PrivacyPolicy = () => {
   return (
     <>
       <Card className={classes.card}>
-        <Typography variant="h5">プライバシーポリシー</Typography>
-        <Box mt={1} />
-        <Typography variant="h6">個人情報の利用目的</Typography>
-        <Typography style={{ maxWidth: "400px" }}>
-          当アプリでは、ユーザー登録の際、メールアドレス等の個人情報を入力いただく場合がございます。
-          取得した個人情報は、当アプリのサービスのために利用させていただくものであり、これらの目的以外では利用いたしません。
-        </Typography>
-        <Box mt={2} />
-        <Typography>最終更新日 2021年7月7日</Typography>
+        <div className={classes.content}>
+          <Typography variant="h5">プライバシーポリシー</Typography>
+          <Box mt={1} />
+          <Typography variant="h6">個人情報の利用目的</Typography>
+          <Typography>
+            当アプリでは、ユーザー登録の際、メールアドレス等の個人情報を入力いただく場合がございます。
+            取得した個人情報は、当アプリのサービスのために利用させていただくものであり、これらの目的以外では利用いたしません。
+          </Typography>
+          <Box mt={2} />
+          <Typography>最終更新日 2021年7月7日</Typography>
+        </div>
       </Card>
     </>
   );
