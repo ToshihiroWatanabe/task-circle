4282081dbc44d435efafbfa092d6d818e07c796b
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-01 21:48:01 +0900

    update: アプリ起動時に実行する処理を削除
    SessionServiceのAutowiredが原因でテストコードを実行できないため

--- springboot/src/main/java/app/taskcircle/Application.java
+++ springboot/src/main/java/app/taskcircle/Application.java
@@ -1,35 +1,12 @@
 package app.taskcircle;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import app.taskcircle.service.SessionService;
 
 @SpringBootApplication
 public class Application {
 
-	private final SessionService sessionService;
-
-	@Autowired
-	public Application(SessionService sessionService) {
-		this.sessionService = sessionService;
-	}
-
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
-		Application app = ctx.getBean(Application.class);
-		System.out.println("deleteAll: " + app.execStartup(args));
-	}
-
-	/**
-	 * アプリケーション起動時に実行する処理です。
-	 * 
-	 * @param args
-	 * @return 成功した場合はtrue
-	 */
-	public boolean execStartup(String[] args) {
-		return sessionService.deleteAll();
+		SpringApplication.run(Application.class, args);
 	}
 }
