e02c3e3708b1c960928046582ef5ecc2687d2bc8
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-19 10:06:16 +0900

    update: .well-knownが含まれるURLでindex.htmlを返さないようにした

--- springboot/src/main/java/app/taskcircle/controller/IndexController.java
+++ springboot/src/main/java/app/taskcircle/controller/IndexController.java
@@ -14,10 +14,10 @@ public class IndexController {
     /**
      * リクエストを受けて、index.htmlを返します。
      * 
-     * @param request APIやWebSocketを除くリクエスト
+     * @param request リクエスト
      * @return index.html
      */
-    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api|websocket$).*$}/**/{y:[\\w\\-]+}", })
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api|websocket|.well-known$).*$}/**/{y:[\\w\\-]+}", })
     public String getIndex(HttpServletRequest request) {
         return "/index.html";
     }
